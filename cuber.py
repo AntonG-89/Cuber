6b7f4a5b50a37b9bfb3f6545cae18dd9f4955675
Anton Goncharenko <63752091+AntonG-89@users.noreply.github.com>
2020-09-24 22:15:10 -0400

    Update cuber.py

--- cuber.py
+++ cuber.py
@@ -3,9 +3,15 @@
 Created on Thu Sep 24 19:31:38 2020
 
 @author: anton
+"This program is meant to help practicing solving cubic roots. The random fucntion inputs a number from 0 to 100, and outputs its cube. 
+It then waits for your solution(cubic root of the output) and evaluates it against the number that was cubed, telling you whether your solution was correct"
+
 """
+
 import time
 import random
+
+
 def cuber():
     
     again =  True
@@ -19,11 +25,12 @@ def cuber():
         print("Here is your cube: %i" %cube)
         
         solve = input("And the winner is: ")
-        time.sleep(3)
+        time.sleep(2)
         if int(solve) == numRand:
             print("You got it, it's: %i" %numRand)
         else:
             print("You dum")
+        #This section needs work for better input handling
         nextRound = input("Press Enter to continue, press Space to quit: ")
         if nextRound == "":
             return again
